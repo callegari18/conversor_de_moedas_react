4068c6a39abb14cafa70a13af4435dbecce80425
Vinicius Callegari <callegari@hotmail.com>
2021-03-02 15:13:53 -0300

    projeto conversor de moedas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Conversor from "./components/Conversor"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="aplicativo">
+
+        <h1>Conversor de moedas</h1>
+        <div className="linha">
+          <div className="euaTobr">
+            <Conversor moedaA="USD" moedaB="BRL"></Conversor>      
+          </div>
+          <div className="brToeua">
+            <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+          </div>
+        </div>
+        <div className="linha">
+          <div className="cadTobr">
+            <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+          </div>
+          <div className="brTocad">
+            <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+          </div>
+        </div>
+        <div className="linha">
+          <div className="eurTobr">
+            <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+          </div>
+          <div className="brToeur">
+            <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+          </div>
+        </div>
+          
+      </div>
     </div>
   );
 }
